66735afb6c6f5f6a4fe053cbfc86aac0804f6349
suyashpradhan <pradhansuyash39@gmail.com>
2024-10-14 16:57:19 +0530

    feat: adds react crud app - post method

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,68 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useId} from 'react';
+import axios from 'axios';
+
+type Post = {
+    id: string,
+    title: string,
+    body: string,
+    userId: string
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [title, setTitle] = React.useState('');
+    const [description, setDescription] = React.useState('');
+    const [posts, setPosts] = React.useState<Post[]>([]);
+    const [loading, setLoading] = React.useState(false);
+
+    const id = useId()
+
+    const handleSubmit = async (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+        e.preventDefault();
+        setLoading(true);
+        try {
+            const response = await axios.post('https://jsonplaceholder.typicode.com/posts', {
+                    id: id,
+                    title: title,
+                    body: description,
+                    userId: id
+                }
+            )
+            setPosts([...posts, response.data]);
+            setLoading(false);
+        } catch (e) {
+            setLoading(false);
+            console.log(e)
+        }
+    }
+    return (
+        <div className="App">
+            <h1>Simple CRUD APP</h1>
+            <form>
+                <input type="text" value={title} placeholder="Title" name="title"
+                       onChange={(event) => setTitle(event.target.value)}/>
+                <input type="text" value={description} placeholder="Description" name="description"
+                       onChange={(event) => setDescription(event.target.value)}/>
+                <button type="submit" onClick={handleSubmit} disabled={loading}>
+                    {loading ? 'Adding...' : 'Add Post'}
+                </button>
+            </form>
+
+            {posts.length === 0 &&
+                <>
+                    <h2>Posts</h2>
+                    <p>No posts available.</p>
+                </>}
+            <ul>
+                {posts.map((post: Post) => (
+                    <div key={post.id}>
+                        <h4>{post.title}</h4>
+                        <p>{post.body}</p>
+                        <hr/>
+                    </div>
+                ))}
+            </ul>
+        </div>
+    );
 }
 
 export default App;
